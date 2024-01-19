856cc59fbfcee73309a0ad5da82eba65e0171672
srivatsav01 <cs20btech11003@iith.ac.in>
2024-01-20 03:25:19 +0530

    Add NFT contract and events

--- KYC-zkgraph/src/mapping.ts
+++ KYC-zkgraph/src/mapping.ts
@@ -1,36 +1,29 @@
 //@ts-ignore
 import { require } from "@hyperoracle/zkgraph-lib";
-import { Bytes, Block, Event } from "@hyperoracle/zkgraph-lib";
+import { Bytes, Block, BigInt, Event } from "@hyperoracle/zkgraph-lib";
 
-let addr = Bytes.fromHexString('0xa60ecf32309539dd84f27a9563754dca818b815e');
-let esig_sync = Bytes.fromHexString("0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1");
+let addr = Bytes.fromHexString('0x3D945FD9Af112b5427BbEC64cF4b2C3811C57D83');
+let esig_sync = Bytes.fromHexString("0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef");
+
+const kycedAddress = "0x36d630c4775c58c9634406e824dfb73a20f5b9a5"
 
 export function handleBlocks(blocks: Block[]): Bytes {
-  // init output state
   let state: Bytes;
 
-  // #1 can access all (matched) events of the latest block
   let events: Event[] = blocks[0].events;
 
-  // #2 also can access (matched) events of a given account address (should present in yaml first).
-  // a subset of 'events'
+ 
   let eventsByAcct: Event[] = blocks[0].account(addr).events;
 
-  // #3 also can access (matched) events of a given account address & a given esig  (should present in yaml first).
-  // a subset of 'eventsByAcct'
   let eventsByAcctEsig: Event[] = blocks[0].account(addr).eventsByEsig(esig_sync)
 
-  // require match event count > 0
+
   require(eventsByAcctEsig.length > 0)
 
-  // this 2 way to access event are equal effects, alway true when there's only 1 event matched in the block (e.g. block# 2279547 on sepolia).
-  require(
-    events[0].data == eventsByAcct[0].data 
-    && events[0].data == eventsByAcctEsig[0].data
-  );
 
-  // set state to the address of the 1st (matched) event, demo purpose only.
-  state = events[0].address;
+  let onChainKYCedAddress: BigInt = BigInt.fromBytes(events[0].topic2);
 
-  return state
+// check if the onchain kyced address is equal to the Kyched address we want 
+  require(Bytes.fromHexString(onChainKYCedAddress.toHexString()) == Bytes.fromHexString(kycedAddress)) ;
+  return Bytes.fromHexString(onChainKYCedAddress.toHexString())
 }
