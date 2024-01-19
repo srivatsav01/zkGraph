b3200ce6f1ad04647a6724b92f8c71ef79af941f
srivatsav01 <cs20btech11003@iith.ac.in>
2024-01-20 02:13:19 +0530

    check recipient

--- proof-of-student-zkgraph/src/mapping.ts
+++ proof-of-student-zkgraph/src/mapping.ts
@@ -1,10 +1,11 @@
 //@ts-ignore
 import { require } from "@hyperoracle/zkgraph-lib";
 import { Bytes, Block, Event, BigInt } from "@hyperoracle/zkgraph-lib";
-export const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; 
+const EASContractAddress = "0xC2679fBD37d54388Ce493F1DB75320D236e1815e"; 
 
 
-
+const AttesterAddress = '0x761a6997F55D31069B0E47a88bBEfb1505Bb7E0f'
+const ReipientAddress = "0x761a6997F55D31069B0E47a88bBEfb1505Bb7E0f"
 let addr = Bytes.fromHexString('0xC2679fBD37d54388Ce493F1DB75320D236e1815e');
 let esig_sync = Bytes.fromHexString("0x8bf46bf4cfd674fa735a3d63ec1c9ad4153f033c290341f3a588b75685141b35");
 
@@ -19,8 +20,18 @@ export function handleBlocks(blocks: Block[]): Bytes {
 
   require(eventsByAcctEsig.length > 0)
 
-  let UID: BigInt = BigInt.fromBytes(events[0].data);
+  let onchainAttester: BigInt = BigInt.fromBytes(events[0].topic1);
+  let onChainRecipient: BigInt = BigInt.fromBytes(events[0].topic2);
 
 
-  return Bytes.fromHexString(UID.toHexString())
+  //check if the onChainAttester is same as the actual attester 
+
+  require(Bytes.fromHexString(onchainAttester.toHexString()) == Bytes.fromHexString(AttesterAddress)) ;
+
+
+
+  // check if the recipeient we need is there. 
+
+  require(Bytes.fromHexString(onChainRecipient.toHexString()) == Bytes.fromHexString(ReipientAddress)) ;
+  return Bytes.fromHexString(onChainRecipient.toHexString())
 }
